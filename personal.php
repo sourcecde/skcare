abd9967f09b7ba979504f82194e62b2fa8dddf55
Debraj Ghosh <debraj.debtech@gmail.com>
2018-01-19 03:06:48 +0530

    change

--- personal.php
+++ personal.php
@@ -26,13 +26,13 @@
 						<div class="service-sidebar">
 							<ul class="service-catergory">
 								<li><a href="#"><span class="icon-left fa fa-chevron-right"></span>Dementia</a></li>
-								<li class="active">
+								<li>
 									<a href="#"><span class="icon-left fa fa-chevron-right"></span>Learning Disabilities</a>
 								</li>
 								<li>
 									<a href="#"><span class="icon-left fa fa-chevron-right"></span>Mental Health Condition</a>
 								</li>
-								<li>
+								<li  class="active">
 									<a href="personal.php"><span class="icon-left fa fa-chevron-right"></span>Personal Care</a>
 								</li>
 								<li>
